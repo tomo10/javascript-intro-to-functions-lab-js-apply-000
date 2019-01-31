4a5efa439bb40688abc664ed47139134c859478e
Thomas Edwards <thce28@gmail.com>
2019-01-31 11:52:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,12 @@ function whisper(string) {
   return string.toLowerCase()
 }
 
+function logShout(string) {
+  console.log(string.toUpperCase()) 
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+
